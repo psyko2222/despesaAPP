dbf3c3317fd2d9705be8c1ab9f31581ad412d964
psyko2222 <luislzandroid@gmail.com>
2026-09-08 17:23:57 +0100

    Ignore linting and TS errors on Vercel build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,14 @@ const formattedUrl = baseUrl.startsWith('http') ? baseUrl : `https://${baseUrl}`
 
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    // Ignora erros de TypeScript durante o build em produção na Vercel
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Ignora avisos/erros do ESLint durante o build em produção na Vercel
+    ignoreDuringBuilds: true,
+  },
   env: {
     NEXT_PUBLIC_API_URL: formattedUrl,
   },
